ad361dc464c9af16daeabdf74907168c3b6f9fee
janahospesova-svg <jana.hospesova@gmail.com>
2026-06-22 15:53:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 const text = {
 
-    cs:{
-    title:"🎮 Moje Hry",
-    play:"Hrát"
-    },
-    
-    en:{
-    title:"🎮 My Games",
-    play:"Play"
-    },
-    
-    de:{
-    title:"🎮 Meine Spiele",
-    play:"Spielen"
-    }
-    
-    };
-    
-    document
-    .getElementById("language")
-    .addEventListener("change", function(){
-    
-    let lang = this.value;
-    
-    document.getElementById("title").innerText =
-    text[lang].title;
-    
-    document.getElementById("play").innerText =
-    text[lang].play;
-    
-    });
+cs:{
+title:"🎮 Moje Hry",
+games:"Minecraft,Roblox"
+play:"Hrát"
+},
+
+en:{
+title:"🎮 My Games",
+games:"Minecraft,Roblox"
+play:"Play"
+},
+
+de:{
+title:"🎮 Meine Spiele",
+games:"Minecraft,Roblox"
+play:"Spielen"
+}
+
+};
+
+document
+.getElementById("language")
+.addEventListener("change", function(){
+
+let lang = this.value;
+
+document.getElementById("title").innerText =
+text[lang].title;
+
+document.getElementById("play").innerText =
+text[lang].play;
+
+});
